a93d9892d09430d47c29cda4bc6731f5823c3cee
sebastien <2le@2le.net>
2018-10-10 08:42:43 +0200

    voter

---
+++ src/Security/CredentialVoter.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Lle\CredentialBundle\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class CredentialVoter extends Voter
+{
+
+    private $decisionManager;
+
+    public function __construct(AccessDecisionManagerInterface $decisionManager)
+    {
+        $this->decisionManager = $decisionManager;
+    }
+
+    protected function supports($attribute, $subject)
+    {
+        // vote on everything
+
+        return true;
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        // if the user is anonymous, do not grant access
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ROLE_SUPER_ADMIN can do anything! The power!
+        if ($this->decisionManager->decide($token, array('ROLE_SUPER_ADMIN'))) {
+            return true;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case 'EDIT':
+                // logic to determine if the user can EDIT
+                // return true or false
+                break;
+            case 'VIEW':
+                // logic to determine if the user can VIEW
+                // return true or false
+                break;
+        }
+
+        return false;
+    }
+}
