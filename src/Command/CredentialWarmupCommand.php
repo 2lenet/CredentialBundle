4c5713b2cc1ff5d0c155688b0923aadd36d37d66
valentin-helies <89925635+valentin-helies@users.noreply.github.com>
2024-06-07 14:17:49 +0200

    Update CredentialWarmupCommand.php

--- src/Command/CredentialWarmupCommand.php
+++ src/Command/CredentialWarmupCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
-use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
 
 #[AsCommand(
     name: 'credential:warmup',
