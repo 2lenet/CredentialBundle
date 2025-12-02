bb04d21c6c709c8cba218e7e16c470fc3b4fa74c
Valentin HÉLIES <valentin@2le.net>
2025-12-02 18:24:42 +0100

    Fix phpstan annotation error

--- src/Controller/LoadCredentialController.php
+++ src/Controller/LoadCredentialController.php
@@ -7,7 +7,7 @@ use Lle\CredentialBundle\Exception\ConfigurationProjectCodeNotDefinedException;
 use Lle\CredentialBundle\Service\LoadCredentialService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class LoadCredentialController extends AbstractController
